a308674705d4378e41d5b84bf4d8662dd394c56c
Nayely Ayol <nayely.ayol@epn.edu.ec>
2026-04-23 01:49:51 -0500

    fix: implementación de mensaje informativo para el módulo dashboard

--- src/features/dashboard/pages/DashboardPage.jsx
+++ src/features/dashboard/pages/DashboardPage.jsx
@@ -9,16 +9,14 @@ import { IoDocuments } from "react-icons/io5"
 export default function DashboardPage() {
 
     return (
-        <div className="flex flex-col gap-6">
+        <div className="flex flex-col gap-6 p-6">
 
-            {/* HEADER */}
             <div>
-                <p className="text-gray-500 text-sm">
-                    Estadísticas generales del sistema
+                <p className="text-gray-500">
+                    En este módulo puede visualizar las estadísticas generales del sistema
                 </p>
             </div>
 
-            {/* 🔹 WIDGETS */}
             <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-6 gap-5">
 
                 <Widget
@@ -59,7 +57,6 @@ export default function DashboardPage() {
 
             </div>
 
-            {/* 🔹 CHARTS */}
             <div className="grid grid-cols-1 xl:grid-cols-2 gap-6">
 
                 <TotalSpentCard />
