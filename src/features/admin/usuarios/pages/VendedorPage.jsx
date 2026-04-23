6e89f8ff8bb4fb158613c0d18132877c1b8b1819
Nayely Ayol <nayely.ayol@epn.edu.ec>
2026-04-23 01:35:48 -0500

    fix: implementación de mensaje informativo del módulo vendedores.

--- src/features/admin/usuarios/pages/VendedorPage.jsx
+++ src/features/admin/usuarios/pages/VendedorPage.jsx
@@ -3,7 +3,13 @@ import VendedorForm from "../components/VendedorForm"
 export default function VendedoresPage() {
 
     return (
-        <div className="space-y-6">
+        <div className="p-6 space-y-6">
+
+            <div>
+                <p className="text-gray-500">
+                    En este módulo puedes crear vendedores 
+                </p>
+            </div>
 
             <div className="bg-white/60 backdrop-blur-xl p-6 rounded-2xl border border-white/20">
                 <h2 className="text-xl font-bold mb-4">
