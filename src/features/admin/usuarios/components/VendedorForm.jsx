ce888c386c05236636c2ff6f5ad66aa0e6ada186
Nayely Ayol <nayely.ayol@epn.edu.ec>
2026-04-28 18:56:56 -0500

    style: actualización de textos en formulario de vendedores

--- src/features/admin/usuarios/components/VendedorForm.jsx
+++ src/features/admin/usuarios/components/VendedorForm.jsx
@@ -106,7 +106,7 @@ export default function VendedorForm({ onSuccess, tipo = "VENDEDOR" }) {
 
             <div className="md:col-span-2">
                 <Button type="submit" disabled={loading} className={buttonPrimaryClass}>
-                    {loading ? "Creando..." : `Crear ${tipo === "VENDEDOR" ? "Vendedor" : "Cliente"}`}
+                    {loading ? "Creando..." : `Crear`}
                 </Button>
             </div>
 
--- src/features/admin/usuarios/pages/VendedorPage.jsx
+++ src/features/admin/usuarios/pages/VendedorPage.jsx
@@ -9,14 +9,11 @@ export default function VendedoresPage({ tipo = "VENDEDOR" }) {
 
             <div>
                 <p className="text-gray-500">
-                    En este módulo puedes crear {isVendedor ? "vendedores" : "clientes"} 
+                    Este módulo te permite crear {isVendedor ? "vendedores" : "clientes"} 
                 </p>
             </div>
 
             <div className="bg-white/60 backdrop-blur-xl p-6 rounded-2xl border border-white/20">
-                <h2 className="text-xl font-bold mb-4">
-                    Crear {isVendedor ? "Vendedor" : "Cliente"}
-                </h2>
 
                 <VendedorForm tipo={tipo}/>
             </div>
