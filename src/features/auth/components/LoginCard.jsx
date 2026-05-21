a46d5b87229a4615e838b12185255969669c63b0
Nayely Ayol <nayely.ayol@epn.edu.ec>
2026-05-21 02:15:02 -0500

    fix: corrección de distribucion de boton y titulo en login

--- src/features/auth/components/LoginCard.jsx
+++ src/features/auth/components/LoginCard.jsx
@@ -3,7 +3,7 @@ import { Card, CardHeader, CardTitle, CardContent, CardFooter } from "@/componen
 import { Button } from "@/components/ui/button"
 import { Label } from "@/components/ui/label"
 import LoginForm from "./LoginForm"
-
+import { FiArrowLeft } from "react-icons/fi"
 import {
     titleClass,
     footerLabelClass,
@@ -15,17 +15,29 @@ export default function LoginCard({ onLogin }) {
 
     return (
         <Card className="w-full max-w-lg border-0 border-transparent bg-transparent shadow-none p-0 outline-none ring-0">
-            <button
-                onClick={() => navigate("/")}
-                className="absolute top-6 left-6 text-sm text-gray-600 hover:text-emerald-600 transition"
-            >
-                ← Volver
-            </button>
-            <CardHeader className="flex justify-center p-0 pb-6">
-                <CardTitle className={titleClass}>
-                    Inicia Sesión
-                </CardTitle>
-            </CardHeader>
+            <div className="relative flex items-center justify-center pt-2 mb-5">
+
+                <button
+                    onClick={() => navigate("/")}
+                    className="
+                        absolute left-0
+                        w-10 h-10 rounded-xl
+                        bg-white shadow-sm
+                        border border-gray-100
+                        flex items-center justify-center
+                        hover:bg-emerald-50 transition
+                    "
+                >
+                    <FiArrowLeft className="text-xl text-gray-700" />
+                </button>
+
+                <CardHeader className="p-0">
+                    <CardTitle className={titleClass}>
+                        Inicia Sesión
+                    </CardTitle>
+                </CardHeader>
+
+            </div>
 
             <CardContent className="p-0">
                 <LoginForm onSubmit={onLogin} />
