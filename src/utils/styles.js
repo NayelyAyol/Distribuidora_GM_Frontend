6651f27eb6da54c220c3977780af29eda5ff950e
Nayely Ayol <nayely.ayol@epn.edu.ec>
2026-06-23 18:40:52 -0500

    fix: correciones generales

--- src/utils/styles.js
+++ src/utils/styles.js
@@ -1,6 +1,6 @@
 // INPUTS & LABELS
 export const inputClass =
-    "w-full py-4 px-6 rounded-2xl bg-white/70 border border-gray-300 outline-none focus:ring-2 focus:ring-emerald-500 focus:border-emerald-500 transition-all duration-200 text-base md:text-lg placeholder-emerald-900/60";
+    "w-full py-4 px-6 rounded-2xl bg-white/70 border border-gray-300 outline-none focus:ring-2 focus:ring-emerald-500 focus:border-emerald-500 transition-all duration-200 text-base md:text-lg placeholder-emerald-900/60 [color-scheme:light]";
 
 export const labelClass =
     "font-bold text-sm text-emerald-950 ml-1";
